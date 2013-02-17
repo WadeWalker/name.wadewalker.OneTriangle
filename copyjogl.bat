1320c8887df42204caa1d2f567dc65e3cfeb1ef0
Wade Walker <wwalker3@austin.rr.com>
2013-02-17 11:02:01 -0600

    Made copyjogl.bat paths relative.

--- copyjogl.bat
+++ copyjogl.bat
@@ -1,7 +1,7 @@
-copy D:\Users\Wade\Documents\Blog\jogl2\gluegen\build\gluegen-java-src.zip D:\Users\Wade\Documents\Blog\jogl2\com.jogamp.jogl
-copy D:\Users\Wade\Documents\Blog\jogl2\gluegen\build\gluegen-rt.jar D:\Users\Wade\Documents\Blog\jogl2\com.jogamp.jogl
-copy D:\Users\Wade\Documents\Blog\jogl2\gluegen\build\gluegen-rt-natives-windows-amd64.jar D:\Users\Wade\Documents\Blog\jogl2\com.jogamp.jogl
+copy ..\gluegen\build\gluegen-java-src.zip ..\com.jogamp.jogl
+copy ..\gluegen\build\gluegen-rt.jar ..\com.jogamp.jogl
+copy ..\gluegen\build\gluegen-rt-natives-windows-amd64.jar ..\com.jogamp.jogl
 
-copy D:\Users\Wade\Documents\Blog\jogl2\jogl\build\jar\jogl-all.jar D:\Users\Wade\Documents\Blog\jogl2\com.jogamp.jogl
-copy D:\Users\Wade\Documents\Blog\jogl2\jogl\build\jar\jogl-all-natives-windows-amd64.jar D:\Users\Wade\Documents\Blog\jogl2\com.jogamp.jogl
-copy D:\Users\Wade\Documents\Blog\jogl2\jogl\build\jogl-java-src.zip D:\Users\Wade\Documents\Blog\jogl2\com.jogamp.jogl
+copy ..\jogl\build\jar\jogl-all.jar ..\com.jogamp.jogl
+copy ..\jogl\build\jar\jogl-all-natives-windows-amd64.jar ..\com.jogamp.jogl
+copy ..\jogl\build\jogl-java-src.zip ..\com.jogamp.jogl
